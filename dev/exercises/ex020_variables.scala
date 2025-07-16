1dc54c91eeb70b23421102bcf5f5ee61f6aac0cc
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-16 05:56:53 +0200

    DEVCONTENT: finish ex020

--- dev/exercises/ex020_variables.scala
+++ dev/exercises/ex020_variables.scala
@@ -1,8 +1,19 @@
 object General:
 
   var age = 20
+  var showAge: Boolean = false
+  var name: String = "Hans" // always use double quotes for String
+  var letter: Char = 'H' // always use single quotes for Char
+  var rank:Float = 3.2f
+  var ranking:Double = 3.2 // more common than Float
 
   def main(args: Array[String]): Unit =
-    println("He is " + age + " years old.")
-
-
+    if (showAge) {
+      println("He is " + age + " years old.")
+    } else {
+      println("didn't show age")
+    }
+    println("Name: " + name)
+    println("Letter: " + letter)
+    println("Rank: " + rank)
+    println("R: " + ranking)
